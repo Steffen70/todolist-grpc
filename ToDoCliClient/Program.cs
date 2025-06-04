33597e7b770493188f4152c298f126ee1d15bfe9
Steffen70 <steffen@seventy.mx>
2025-06-05 00:56:54 +0200

    Added Program class to Program.cs minimal CLI spaghetti.

--- ToDoCliClient/Program.cs
+++ ToDoCliClient/Program.cs
@@ -2,41 +2,46 @@ using System.Security.Cryptography.X509Certificates;
 using Grpc.Net.Client;
 using ToDoGrpc;
 
-var baseAddress = new Uri("https://localhost:8443");
+namespace ToDoCliClient;
 
-// Load root CA
-using var rootCa = new X509Certificate2(Path.Combine(AppContext.BaseDirectory, "cert", "root_ca.crt"));
-
-// Create handler that trusts your custom cert
-var handler = new HttpClientHandler();
-handler.ServerCertificateCustomValidationCallback = (_, cert, chain, __) =>
+public static class Program
 {
-    // Manually trust your own root CA
-    chain!.ChainPolicy.ExtraStore.Add(rootCa);
-    chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
-    chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
+    internal static void Main()
+    {
+        var baseAddress = new Uri("https://localhost:8443");
 
-    var isValid = chain.Build(cert!);
-    return isValid;
-};
+        // Load root CA
+        using var rootCa = new X509Certificate2(Path.Combine(AppContext.BaseDirectory, "cert", "root_ca.crt"));
 
-// Create gRPC channel using custom handler
-var httpClient = new HttpClient(handler);
-var channel = GrpcChannel.ForAddress(baseAddress, new()
-{
-    HttpClient = httpClient
-});
+        // Create handler that trusts your custom cert
+        var handler = new HttpClientHandler();
+        handler.ServerCertificateCustomValidationCallback = (_, cert, chain, __) =>
+        {
+            // Manually trust your own root CA
+            chain!.ChainPolicy.ExtraStore.Add(rootCa);
+            chain.ChainPolicy.VerificationFlags = X509VerificationFlags.AllowUnknownCertificateAuthority;
+            chain.ChainPolicy.RevocationMode = X509RevocationMode.NoCheck;
 
-// Use your client
-var todoClient = new todoit.todoitClient(channel);
+            var isValid = chain.Build(cert!);
+            return isValid;
+        };
 
-todoClient.CreateList(new() { ListName = "List from CLI" });
+        // Create gRPC channel using custom handler
+        var httpClient = new HttpClient(handler);
+        var channel = GrpcChannel.ForAddress(baseAddress, new()
+        {
+            HttpClient = httpClient
+        });
 
-var result = todoClient.ReadLists(new());
+        // Use your client
+        var todoClient = new todoit.todoitClient(channel);
 
-foreach (var todoList in result.Lists)
-{
-    Console.WriteLine($"Found list: {todoList.ListName}");
+        todoClient.CreateList(new() { ListName = "List from CLI" });
+
+        var result = todoClient.ReadLists(new());
+
+        foreach (var todoList in result.Lists) Console.WriteLine($"Found list: {todoList.ListName}");
+
+        Console.ReadLine();
+    }
 }
-
-Console.ReadLine();
