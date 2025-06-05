b13e9fb521609dd94f0885ad9c7bdfa456d9289a
NaderCc <kanmagdy@gmail.com>
2025-06-05 18:16:19 +0300

    Edit ToDoCliClient and put CreateList&Add Item

--- ToDoCliClient/Program.cs
+++ ToDoCliClient/Program.cs
@@ -1,4 +1,5 @@
 using System.Security.Cryptography.X509Certificates;
+using Grpc.Core;
 using Grpc.Net.Client;
 using ToDoGrpc;
 
@@ -30,13 +31,81 @@ var channel = GrpcChannel.ForAddress(baseAddress, new()
 // Use your client
 var todoClient = new todoit.todoitClient(channel);
 
-todoClient.CreateList(new() { ListName = "List from CLI" });
 
-var result = todoClient.ReadLists(new());
 
-foreach (var todoList in result.Lists)
+Console.WriteLine("Hello In ToDoList-App" +
+    "\n1->To create a new list" +
+    "\n2->To Add new Item to a List" +
+    "\n3-To Update An Item" +
+    "\n4-To Delete Item");
+int Choice = int.Parse(Console.ReadLine());
+
+switch(Choice)
 {
-    Console.WriteLine($"Found list: {todoList.ListName}");
+    //Create List 1 
+    case 1:
+        {
+            Console.WriteLine("You have to Enter List_name:");
+            string list_name = Console.ReadLine();
+            todoClient.CreateList(new() { ListName = list_name });
+            var result = todoClient.ReadLists(new()).Lists.FirstOrDefault(x=>x.ListName ==list_name);
+
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("|{0,-10} | {1,-5} |", "ListId", "ListName");
+            Console.WriteLine(new string('-', 20));
+            Console.WriteLine("|{0,-10} | {1,-5} |", result.Id , result.ListName);
+            Console.WriteLine(new string('-', 20));
+            break;
+        }
+    case 2:
+        {
+            Console.WriteLine("Enter Id of List:");
+            Int32 list_id = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter ItemName:");
+            string item_name = Console.ReadLine();
+            var result = todoClient.ReadLists(new());
+            if (result.Lists.FirstOrDefault(x => x.Id == list_id) == null)
+            {
+                throw new RpcException(new Status(StatusCode.InvalidArgument, "can't find this one"));
+                
+            }
+            else
+            {
+                todoClient.AddItemToList(new()
+                {
+                    ToDoListId = list_id,
+                    ItemName = item_name,
+                });
+                Console.WriteLine("--->>>>>Item is added!<<<<<---");
+                break;
+            }
+            
+        }
+    default: 
+            {
+
+            break;
+            }
 }
+//Add Item 2 
+
+//Read Item 3 
+
+// Delete Item
+
+//Update Item 
+
+
+//MarkIsDone
+
+//todoClient.CreateList(new() { ListName = "List from CLI" });
+
+
+//var result = todoClient.ReadLists(new());
+
+//foreach (var todoList in result.Lists)
+//{
+//    Console.WriteLine($"Found list: {todoList.ListName}");
+//}
 
 Console.ReadLine();
