5aaa163cf8bc9543a9ffd3acda44f93f7481e857
Assessment Candidate <app@swisspension.net>
2025-07-17 10:42:00 +0200

    Replaced greet.proto with todo.proto in python stubs generator script.

--- todo_flask_client/generate_grpc_stubs.sh
+++ todo_flask_client/generate_grpc_stubs.sh
@@ -16,11 +16,11 @@ pipenv run python -m grpc_tools.protoc \
   -I "$DIR/../protos" \
   --python_out="$OUTDIR" \
   --grpc_python_out="$OUTDIR" \
-  "$DIR/../protos/greet.proto"
+  "$DIR/../protos/todo.proto"
 
 # Fix imports using protoletariat
 pipenv run protol \
   --create-package \
   --in-place \
   --python-out "$OUTDIR" \
-  protoc --proto-path="$DIR/../protos" "$DIR/../protos/greet.proto"
+  protoc --proto-path="$DIR/../protos" "$DIR/../protos/todo.proto"
